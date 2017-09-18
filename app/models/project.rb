f2381710b5a8a683c635aa95c68eae942cb1daae
Andy Tran <andy8tran@gmail.com>
2017-09-18 14:48:04 -0400

    project must start in future

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,11 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :title, :description, :goal, :start_date, :user_id, :end_date, presence: true
+  validate :start_date_canot_be_in_past
+
+  def start_date_canot_be_in_past
+    if start_date < Date.today
+      errors.add(:start_date, "cannot be in the past.")
+    end
+  end
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -20,6 +20,19 @@ class ProjectTest < ActiveSupport::TestCase
     assert project.invalid?, 'Project should not save without owner.'
   end
 
+  test 'project must start in future' do
+
+    project = new_project
+    user =  new_user
+    user.save
+    project.user = user
+    project.start_date = Date.today - 1.week
+    project.save
+
+    assert project.invalid?, 'Project should not be able start in the past.'
+
+  end
+
   def new_project
     Project.new(
       title:       'Cool new boardgame',
