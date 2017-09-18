58d2fcf6cd9eb9ab4a67368c0f99ffc94df6c45f
sbcreates <drawsarahb@gmail.com>
2017-09-18 14:27:02 -0400

    Fixed project cannot be backed by owner test

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,12 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner_cannot_back
+
+  def owner_cannot_back
+    if self.user == project.user
+      errors.add(:owner, "cannot back own project")
+    end
+  end
+
 end
