56f8963a2edbb6da604e9eadea89e9f66f3b375e
sbcreates <drawsarahb@gmail.com>
2017-09-21 12:59:09 -0400

    made emails random

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,13 +2,13 @@ FactoryGirl.define do
   factory :user do
     password "helloworld"
     password_confirmation "helloworld"
-    email "asdf@asdf.com"
+    email {rand(20000)}.to_s + "@asdf.com"
     admin false
     first_name "marlon"
     last_name "o'neil"
 
     factory :user2 do
-      email "asdasdff@asasdfdf.com"
+      email {rand(20000)}.to_s + "@asdsafsdaff.com"
       admin false
       first_name "project_owner"
       last_name "o'neil2"
